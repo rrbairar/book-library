40e78f5d48e2552336f4705309f27bd938ed6c52
Ram <rrbairar@gmail.com>
2021-06-28 23:34:44 +0530

    CHANGES IN SERVER JS

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const serveStatic = require('serve-static')
-const path = require('path')
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8082;
 const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use("/", serveStatic(path.join(__dirname, '/dist')))
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.listen(port);
