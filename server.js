4ceb8a9bfb43011bc20b06ca0e76bbd745c3028d
Ram <rrbairar@gmail.com>
2021-06-29 07:40:22 +0530

    HTTP FIX

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT || 8082;
 const app = express();
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use("/", serveStatic(path.join(__dirname, '/dist')))
